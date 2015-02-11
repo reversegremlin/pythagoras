dd2855b693cf54242ee7b285f8bae205ea339537
reversegremlin <reversegremlin@gmail.com>
2015-02-11 15:13:54 -0800

    R1.11

--- primer.py
+++ primer.py
@@ -122,13 +122,19 @@ print(range(50, 90, +10))
 # what parameters should be sent to the range constructor to produce a range with values
 # 8, 6, 4, 2, 0, -2, -4, -6, -8
 
-print(range(-10))
+print(range(8, -10, -2))
 
 
 # R-1.11
+# Demonstrate how to use Python's list comprehension syntax to produce the list
+# [1, 2, 4, 8, 16, 32, 64, 128, 256].
+
+print([pow(2, k) for k in range(0, 9, +1)])
 
 
 # R-1.12
 
-
-
+# Python's random module includes a function choice(data) that returns a random element
+# from a non-empty sequence. The random module in- cludes a more basic function randrange,
+# with parameterization similar to the built-in range function, that return a random choice
+# from the given range. Using only the randrange function, implement your own version of the choice function.
