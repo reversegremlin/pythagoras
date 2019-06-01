8467b9408230356e86612e9a18bb2430db73033a
starkpa <49409857+starkpa@users.noreply.github.com>
2019-06-01 15:23:10 +0530

    Update primer.py
    solution for C-1.25 problem

--- primer.py
+++ primer.py
@@ -300,6 +300,21 @@ for c in given_string:
 
 print(count)
 
+# C-1.25
+#WriteashortPython function thattakes astrings,representing asentence,
+#and returns a copy of the string with all punctuation removed. For example,
+#if given the string "Let s try, Mike.", this function would return "Lets try Mike".
+#
+given_string = 'Let\'s try, Mike.'
+
+punctuations = ''''~!@#$%^*()_-{}[]|\:'";<>,./'''  # Take help of your keyboard to remember this.
+
+newstring = ''
+for i in given_string:
+    if i not in punctuations:
+        newstring += i
+
+print(newstring)
 
 
 
